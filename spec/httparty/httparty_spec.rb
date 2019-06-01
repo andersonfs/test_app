e0faff99b5429a38130950eeecf82a7b342a10fb
anderson.firmino <anderson.firmino@m4u.com.br>
2019-06-01 00:25:16 -0300

    Modified VCR record mode to new episodes.

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,8 +1,8 @@
 describe 'HttParty' do
-  it 'HttParty', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+  it 'HttParty', vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes } do
     #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #       to_return(status: 200, body: "", headers: {'content_type':'application/json; charset: UTF-8'})
-    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5].sample}")
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/4")
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
